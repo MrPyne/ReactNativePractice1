514ede7453ea3530bf88b4e0ad850afe8b8d983e
matts <matt.pyne@hotmail.com>
2017-07-23 10:38:25 -0300

    Trying to setup flow.

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <Text>Hello World!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+        <Greeting name='Matt' />
+      </View>
     );
   }
 }
